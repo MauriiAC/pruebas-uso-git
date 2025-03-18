26d9a5b97bc8bd9c90947b9f262bb7e83b608cfe
mauricio.cuello <mauricio.cuello@coderhouse.com>
2025-03-17 22:11:09 -0300

    se agrega edad a perro

--- animales.py
+++ animales.py
@@ -1,5 +1,6 @@
 class Perro():
 
-    def __init__(self, nombre, raza):
+    def __init__(self, nombre, raza, edad):
         self.nombre = nombre
         self.raza = raza
+        self.edad = edad
